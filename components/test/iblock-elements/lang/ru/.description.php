46ba58e6c91a133e29553f2b8b230663eef16ae9
Ulitin Evgeny <akira-ul@yandex.ru>
2019-07-29 18:39:26 +0300

    Исправлено задание по битриксу, реализация изменена из компонента на класс

--- components/test/iblock-elements/lang/ru/.description.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS ['IBLOCK_ELEMENTS_LIST'] = "Список элементов инфоблока";
-$MESS ['IBLOCK_ELEMENTS_LIST_DESC'] = "Список элементов инфоблока с сортировкой и фильтрацией";
-$MESS ['IBLOCK_COMPONENT'] = "Тестовы компоненты";
-?>
